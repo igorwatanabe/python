4a1d57f3acc36b1fb3608c4a7db2d920de3a5cca
Igor <eng.igorwatanabe@gmail.com>
2023-12-21 15:52:41 -0300

    dia 3.2 de python finalizado

---
+++ seção-03-Flask/dia-02-Ambiente-e-primeira-API/projeto-flask/src/app.py
@@ -0,0 +1,35 @@
+# -from flask import Flask, jsonify
+from flask import Flask
+from controllers.jokes_controller import jokes_controller
+from controllers.musics_controller import musics_controller
+
+# -import random
+from os import environ
+from waitress import serve
+
+app = Flask(__name__)
+app.register_blueprint(jokes_controller, url_prefix="/jokes")
+app.register_blueprint(musics_controller, url_prefix="/musics")
+
+# Removeremos nosso exemplo estático
+# -joke_list = [
+# -  "Por que o bombeiro não gosta de andar? <br> Porque ele socorre.",
+# -  "Sabe como chama a sorveteria do Michel Teló? <br> Ice te Pego.",
+# -  "Por que o espanador não luta caratê? <br> Porque ele luta capoeira"
+# -]
+# -@app.route("/api/joke")
+# -def joke():
+# -    return jsonify({'joke': random.choice(joke_list)})
+
+
+def start_server(host: str = "0.0.0.0", port: int = 8000):
+    if environ.get("FLASK_ENV") == "dev":
+        # Servidor de desenvolvimento do Kit Werkzeug
+        return app.run(debug=True, host=host, port=port)
+    else:
+        # Este é o waitress, otimizado para produção
+        serve(app, host=host, port=port)
+
+
+if __name__ == "__main__":
+    start_server()
